51ca4ce9d640574417009fcb08935ba3fc75e333
Dylan Widjaja <dwidjaja945@gmail.com>
2021-04-27 23:29:15 -0700

    [format]

--- src/views/_Private/Dashboard.vue
+++ src/views/_Private/Dashboard.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Dashboard</h1>
+    <h1>Dashboard</h1>
 </template>
 
 <script lang="ts">
@@ -14,22 +14,20 @@ const db = firebase.firestore();
 
 export default defineComponent({
   created() {
-    firebase.auth().onAuthStateChanged(async firebaseUser => {
+    firebase.auth().onAuthStateChanged(async (firebaseUser) => {
       if (firebaseUser) {
         const { uid } = firebaseUser;
         this.$store.dispatch('setUid', { uid });
         const { year, month } = getToday();
 
-        const user = await db.collection('users')
-          .doc(uid);
-        const test = user.onSnapshot(snap => {
+        const user = await db.collection('users').doc(uid);
+        const test = user.onSnapshot((snap) => {
           console.log(snap);
           debugger;
         });
         debugger;
-        console.log(batch);
-      };
+      }
     });
-  }
+  },
 });
 </script>
